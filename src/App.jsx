dbaeee71f2f57b9607efc68a999132a54313d8ba
Aleix Morte <alemosa94@gmail.com>
2024-02-15 20:30:54 +0100

    added html video

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,37 @@ import "@vidstack/react/player/styles/default/layouts/video.css";
 import { MediaPlayer, MediaProvider } from "@vidstack/react";
 import { defaultLayoutIcons, DefaultVideoLayout } from "@vidstack/react/player/layouts/default";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-    const storageId = "3aeecea8-1035-45a7-8afb-88123b49f84a";
+    const [videoType, setVideoType] = useState("vidstack");
+    const storageId = "cfecb9f1-6f5e-48cc-a78f-4d174acf5883";
     const videoUrl = `https://cdn.rackety.tv/${storageId}/original`;
     //const previewUrl = `https://cdn.rackety.tv/${storageId}/preview.webp`;
     return (
         <>
             <h1>Vidstack playground</h1>
             <div>
-                <MediaPlayer
-                    title="Sprite Fight"
-                    src={{ src: videoUrl, type: "video/mp4" }}
-                    aspectRatio="16/9"
-                    load="visible"
-                    streamType={"on-demand"}
-                >
-                    <MediaProvider />
-                    <DefaultVideoLayout icons={defaultLayoutIcons} />
-                </MediaPlayer>
+                {videoType === "vidstack" && (
+                    <MediaPlayer
+                        title="Sprite Fight"
+                        src={{ src: videoUrl, type: "video/mp4" }}
+                        aspectRatio="16/9"
+                        load="visible"
+                        streamType={"on-demand"}
+                    >
+                        <MediaProvider />
+                        <DefaultVideoLayout icons={defaultLayoutIcons} />
+                    </MediaPlayer>
+                )}
+                {videoType === "html" && (
+                    <video width={800} height={450} controls>
+                        <source src={videoUrl} type={"video/mp4"}></source>
+                    </video>
+                )}
             </div>
+            <button onClick={() => setVideoType("vidstack")}>Vidstack</button>
+            <button onClick={() => setVideoType("html")}>HTML</button>
         </>
     );
 }
